f8a7842191c223e47772c8f121653df146409cf5
RaymondKirk <ray.tunstill@live.co.uk>
2018-06-04 12:54:08 +0100

    Implemented new start thread entry points and virtual destructor

--- include/ThreadClass.hpp
+++ include/ThreadClass.hpp
@@ -1,16 +1,27 @@
 #ifndef STRAWBERRYDATA_THREADCLASS_H
 #define STRAWBERRYDATA_THREADCLASS_H
 
-
 #include <thread>
 #include <mutex>
+
+/// Usage:
+///     Should be derived and both functions below should be implemented in the child class
+///             virtual const void Setup();
+///             virtual const void Loop();
+///     Thread can be started by calling ::Start, alternately you can define custom entry point with ::Start(_t, _tp)
+/// class Example : public ThreadClass {
+///    const void Setup() override {Loop();};
+///    const void Loop() override {};
+/// };
 
 class ThreadClass {
 public:
     explicit ThreadClass(unsigned int hz = 60);
-    ~ThreadClass();
-    virtual const bool ThreadAlive();
-private:
+
+    virtual ~ThreadClass();
+    const bool ThreadAlive();
+
+protected:
     //Thread parameters
     std::thread thread_;
     bool cancel_thread_ = false;
@@ -20,10 +31,15 @@ private:
     virtual const void Setup();
     virtual const void Loop();
 
-    //Thread timers
+    //Thread timers for extended implementation
     size_t refresh_rate_ = 100;
     std::chrono::system_clock::time_point start_, end_;
     long long int thread_timeout_, ms_timeout_;
+
+    //Initialisation Parameters
+    const bool StartThread();
+    template<typename _Function_ref, typename _Scope>
+    const bool StartThread(_Function_ref &&__f, _Scope __scope);
 };
 
 
--- src/ThreadClass.cpp
+++ src/ThreadClass.cpp
@@ -2,13 +2,25 @@
 #include "ThreadClass.hpp"
 
 ThreadClass::ThreadClass(unsigned int hz) : refresh_rate_(hz), ms_timeout_(1000 / hz) {
-
+    //Start(&ThreadClass::Setup, this); Usage in derived class
 }
 
 ThreadClass::~ThreadClass() {
     // Thread terminated when class goes out of scope
     cancel_thread_ = true;
-    thread_.join();
+    if(thread_.joinable())
+        thread_.join();
+}
+
+const bool ThreadClass::StartThread()
+{
+    thread_ = std::thread(std::bind(&ThreadClass::Setup, this));
+}
+
+template<typename _Function_ref, typename _Scope>
+const bool ThreadClass::StartThread(_Function_ref &&__f, _Scope __scope)
+{
+    thread_ = std::thread(std::bind(__f, __scope));
 }
 
 const bool ThreadClass::ThreadAlive() {
@@ -16,7 +28,7 @@ const bool ThreadClass::ThreadAlive() {
 }
 
 const void ThreadClass::Setup() {
-    std::cerr << "No implementation provided for ThreadClass::Setup() exiting thread" << std::endl;
+    std::cerr << "No implementation provided for ThreadClass::Setup() calling ::Loop()" << std::endl;
     Loop();
 }
 
