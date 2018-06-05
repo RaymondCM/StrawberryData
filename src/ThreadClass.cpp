14ae0deefe380847c9b3c3bfb70881af601f46eb
RaymondKirk <ray.tunstill@live.co.uk>
2018-06-05 12:12:05 +0100

    Implemented better thread class example

--- src/ThreadClass.cpp
+++ src/ThreadClass.cpp
@@ -24,14 +24,34 @@ const bool ThreadClass::StartThread(_Function_ref &&__f, _Scope __scope)
 }
 
 const bool ThreadClass::ThreadAlive() {
-    return !cancel_thread_;
+    return !cancel_thread_ && thread_.joinable();
 }
 
 const void ThreadClass::Setup() {
-    std::cerr << "No implementation provided for ThreadClass::Setup() calling ::Loop()" << std::endl;
-    Loop();
+    while (ThreadAlive()) {
+        try {
+            //Wrap the image capture logic around two time points
+            start_ = std::chrono::system_clock::now();
+            std::cerr << "No implementation provided for ThreadClass::Setup() calling ::Loop()" << std::endl;
+            Loop();
+            end_ = std::chrono::system_clock::now();
+
+            //Calculate how long the thread should sleep for
+            // e.g. if Loop() took 4ms and freq is 100Hz then only sleep for 6ms instead of 10ms
+            thread_timeout_ = ms_timeout_ - std::chrono::duration_cast<std::chrono::milliseconds>(end_ - start_).count();
+
+            //Refresh Every N(Hz) by calculating timeout in ms - time took to execute (if < 0 then = 0)
+            if (thread_timeout_ >= 0)
+                std::this_thread::sleep_for(std::chrono::milliseconds(thread_timeout_));
+
+        } catch (const std::exception &err) {
+            std::cerr << "Error: " << err.what() << std::endl;
+            cancel_thread_ = true;
+        }
+    }
 }
 
 const void ThreadClass::Loop() {
     std::cerr << "No implementation provided for ThreadClass::Loop() exiting thread" << std::endl;
+    cancel_thread_ = true;
 }
