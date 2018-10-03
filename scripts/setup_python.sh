4e7ce6dc73e4a2e52879795ae788b66b8ec9ff3e
RaymondKirk <ray.tunstill@live.co.uk>
2018-10-03 19:23:34 +0100

    Implemented better python virtual environment setup

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
 cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
-python3 -m virtualenv venv
+
+package="python3-virtualenv"
+if [ "" == "$(dpkg-query -W --showformat='${Status}\n' $package | grep "ok installed")" ]; then
+  echo "Package $package is not installed. Installing ..."
+  sudo apt-get --assume-yes install $package
+fi
+
+python3 -m virtualenv -p python3 venv
 source venv/bin/activate
-pip install pyowm
+pip install -r requirements.txt
+pip install virtualenvwrapper
+source venv/bin/virtualenvwrapper.sh
+add2virtualenv $(pwd)
+echo $(pwd) > venv/.project
+echo "You should upgrade the package requirements file as necessary. Via pip freeze > requirements.txt"
