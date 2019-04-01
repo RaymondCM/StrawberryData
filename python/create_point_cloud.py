606339b1901443fd2bb385935ba2cf0e2df50fb9
lelouedec <justin.lelouedec@gmail.com>
2019-04-01 16:35:54 +0100

    creation of pointcloud working bro

--- python/create_point_cloud.py
+++ python/create_point_cloud.py
@@ -3,6 +3,7 @@ import png
 import open3d as opend
 import sys
 from PIL import Image
+import matplotlib.pyplot as plt
 
 path = sys.argv[1]
 depth_raw  =  png.Reader(path+"/aligned_depth.png")
@@ -10,19 +11,24 @@ columnCount, rowCount, pngData, metaData = depth_raw.asDirect()
 bitDepth=metaData['bitdepth']
 planeCount = metaData['planes']
 depth_raw = np.vstack(list(pngData))
+print(np.amax(depth_raw))
+print(np.amin(depth_raw))
 
 
-color = Image.open(path+"/rgb.png")
+color = np.asarray(Image.open(path+"/rgb.png"))
+color = color[:,:,::-1]/255.0
+
 color_focal_length = (1386.075684, 1384.652222)
 color_principal_point = ( 947.828674, 554.063538)
 
 points = []
 colors = []
+print(depth_raw.shape)
 for depth_y in range(0,rowCount):
     for depth_x in range(0, columnCount ):
         ## depth to meters
-        z =  depth_raw[depth_y][depth_x]  / 1000
-        if(z!=0 and z <= 0.80):
+        z =  depth_raw[depth_y][depth_x]  / 1000.0
+        if(z!=0 and z <= 1.0):
             x = (depth_x  - color_principal_point[0] ) * z  /  color_focal_length[0]
             y = (depth_y  - color_principal_point[1] ) * z  /  color_focal_length[1]
             point = np.array([x,y,z,1.0])
