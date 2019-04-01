418ebd9bffdb2c54293a6fcf7f835caf98d849b4
lelouedec <justin.lelouedec@gmail.com>
2019-04-01 16:02:09 +0100

    tool  to create point cloud

---
+++ python/create_point_cloud.py
@@ -0,0 +1,45 @@
+import numpy as np
+import png
+import open3d as opend
+import sys
+from PIL import Image
+
+path = sys.argv[1]
+depth_raw  =  png.Reader(path+"/aligned_depth.png")
+columnCount, rowCount, pngData, metaData = depth_raw.asDirect()
+bitDepth=metaData['bitdepth']
+planeCount = metaData['planes']
+depth_raw = np.vstack(list(pngData))
+
+
+color = Image.open(path+"/rgb.png")
+color_focal_length = (1386.075684, 1384.652222)
+color_principal_point = ( 947.828674, 554.063538)
+
+points = []
+colors = []
+for depth_y in range(0,rowCount):
+    for depth_x in range(0, columnCount ):
+        ## depth to meters
+        z =  depth_raw[depth_y][depth_x]  / 1000
+        if(z!=0 and z <= 0.80):
+            x = (depth_x  - color_principal_point[0] ) * z  /  color_focal_length[0]
+            y = (depth_y  - color_principal_point[1] ) * z  /  color_focal_length[1]
+            point = np.array([x,y,z,1.0])
+            point = point[:3]/ point[3]
+            points.append(point * np.array([1,-1,-1]))
+            colors.append(color[depth_y][depth_x])
+print(np.array(points).shape)
+points = np.array(points)
+colors = np.array(colors)
+xcoords = [p[0] for p in points]
+ycoords = [p[1] for p in points]
+zcoords = [p[2] for p in points]
+nb = len(xcoords)
+centroid = np.array([np.sum(xcoords) / nb, np.sum(ycoords) / nb, np.sum(zcoords) / nb])
+points = points - centroid
+pcd = opend.PointCloud()
+pcd.points = opend.Vector3dVector(points)
+pcd.colors = opend.Vector3dVector(colors)
+newptc = opend.voxel_down_sample(pcd, voxel_size = 0.0024 )
+opend.draw_geometries([newptc])
